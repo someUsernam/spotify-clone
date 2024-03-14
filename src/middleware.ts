e52e6fa88d0bd8a878b3655e17a0a03fbc961089
Artur Wojtowicz <arturwojtowik@gmail.com>
2024-03-14 18:14:56 +0100

    update all next and side dependecies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,21 @@
-// import { DEV_URL } from "@/utils/constants";
-// import { cookies } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-// export async function middleware(request: NextRequest) {
-// 	const cookieStore = cookies();
-// 	const expires_in = Number(cookieStore.get("expires_in")?.value);
-// 	const creation_time = Number(cookieStore.get("creation_time")?.value);
+export async function middleware(request: NextRequest) {
+	// const cookieStore = cookies();
+	// const expires_in = Number(cookieStore.get("expires_in")?.value);
+	// const creation_time = Number(cookieStore.get("creation_time")?.value);
+	// const { pathname } = new URL(request.url);
+	// if (
+	// 	Date.now() > creation_time + expires_in * 1000 &&
+	// 	pathname !== "/api/refresh"
+	// ) {
+	// 	console.log("refreshing token");
+	// 	return NextResponse.redirect(`${DEV_URL}/api/refresh`);
+	// }
+	// console.log("not refreshing token");
+	// return NextResponse.next();
+}
 
-// 	const { pathname } = new URL(request.url);
-
-// 	if (
-// 		Date.now() > creation_time + expires_in * 1000 &&
-// 		pathname !== "/api/refresh"
-// 	) {
-// 		console.log("refreshing token");
-// 		return NextResponse.redirect(`${DEV_URL}/api/refresh`);
-// 	}
-
-// 	console.log("not refreshing token");
-// 	return NextResponse.next();
-// }
-
-// export const config = {
-// 	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
