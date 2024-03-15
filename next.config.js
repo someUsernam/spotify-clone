e0ace1411ef331660d9c13b3adc6438265cd273c
Artur Wojtowicz <arturwojtowik@gmail.com>
2024-03-15 10:17:30 +0100

    fix: delete old comments

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		// domains: [
-		// 	"i.scdn.co",
-		// 	"mosaic.scdn.co",
-		// 	"newjams-images.scdn.co",
-		// 	"image-cdn-ak.spotifycdn.com",
-		// 	"image-cdn-fa.spotifycdn.com",
-		// 	"t.scdn.co",
-		// 	"charts-images.scdn.co",
-		// 	"thisis-images.spotifycdn.com",
-		// 	"seed-mix-image.spotifycdn.com",
-		// 	"seeded-session-images.scdn.co",
-		// 	"daily-mix.scdn.co",
-		// 	"dailymix-images.scdn.co",
-		// 	"lineup-images.scdn.co",
-		// ],
 		remotePatterns: [
 			{
 				protocol: "https",
