9882dfd571d1cf1807ddef49cffa9f21a33cba32
Artur Wojtowicz <arturwojtowik@gmail.com>
2024-03-19 18:45:13 +0100

    Update TopBarLayout styling

--- src/app/_components/TopBar/TopBarLayout.tsx
+++ src/app/_components/TopBar/TopBarLayout.tsx
@@ -1,6 +1,6 @@
 function TopBarLayout({ children }: ChildrenProps) {
 	return (
-		<nav className="flex w-full h-16 rounded-ss-lg z-10 py-4 px-5 sticky top-0 justify-between items-center dark:bg-darken-40 gap-x-2">
+		<nav className="flex w-full h-16 rounded-ss-lg z-10 py-4 px-5 sticky top-0 justify-between items-center dark:bg-secondary gap-x-2">
 			{children}
 		</nav>
 	);
