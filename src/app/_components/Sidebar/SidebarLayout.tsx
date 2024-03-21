424880cb6932f4048d0ae3813856c5025b2c1293
Artur Wójtowicz <101038373+someUsernam@users.noreply.github.com>
2024-03-21 14:22:35 +0100

    Update SidebarLayout component width (#30)
    Co-authored-by: Artur Wojtowicz <arturwojtowik@gmail.com>

--- src/app/_components/Sidebar/SidebarLayout.tsx
+++ src/app/_components/Sidebar/SidebarLayout.tsx
@@ -6,7 +6,7 @@ function SidebarLayout({ children }: ChildrenProps) {
 	const { sidebarRef, handleMousedown } = useResize();
 
 	return (
-		<aside className="flex w-1/4 overflow-hidden" ref={sidebarRef}>
+		<aside className="flex w-80 overflow-hidden" ref={sidebarRef}>
 			<div className="@container flex flex-1 flex-col gap-y-2">{children}</div>
 			<div
 				className="w-1 justify-end resize-x cursor-col-resize dark:hover:bg-gray-700"
