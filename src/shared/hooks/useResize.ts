b39775859594d31fb5644c9b146f6a22d6e88f26
Artur Wójtowicz <101038373+someUsernam@users.noreply.github.com>
2024-05-02 17:29:20 +0200

    REFACTOR: Refactor useResize hook to improve performance and add localStorage support (#45)
    Co-authored-by: Artur Wojtowicz <arturwojtowik@gmail.com>

--- src/shared/hooks/useResize.ts
+++ src/shared/hooks/useResize.ts
@@ -1,53 +1,46 @@
-import { useCallback, useRef } from "react";
+import { useEffect, useRef } from "react";
 
-const REFRESH_WITH_20FPS = 1000 / 20;
+const REFRESH_RATE = 1000 / 30;
+const MIN_WIDTH = 200;
+let MAX_WIDTH = 500;
+if (typeof window !== "undefined") {
+	MAX_WIDTH = window.innerWidth / 2;
+}
 
 function useResize() {
 	const isResizing = useRef(false);
 	const sidebarRef = useRef<HTMLElement>(null);
 	const lasTime = useRef(0);
 
-	// const width = Number(localStorage.getItem("sidebarWidth"));
+	useEffect(() => {
+		const width = Number(localStorage.getItem("sidebarWidth"));
+		if (width && sidebarRef.current) {
+			sidebarRef.current.style.width = `${width}px`;
+		}
+	}, []);
 
-	// if (width && sidebarRef.current) {
-	// 	sidebarRef.current.style.width = `${width}px`;
-	// }
-
-	// useEffect(() => {
-	// 	if (!sidebarRef.current) {
-	// 		console.log("sidebarRef.current is null");
-	// 		return;
-	// 	}
-	// 	sidebarRef.current.style.width = `${sidebarWidth}px`;
-	// }, [sidebarWidth]);
-
-	// console.log(width);
-
-	const handleMouseMove = useCallback((e: MouseEvent) => {
+	const handleMouseMove = (e: MouseEvent) => {
 		if (!isResizing.current || !sidebarRef.current) {
 			return;
 		}
 
 		const now = Date.now();
-		if (now - lasTime.current < REFRESH_WITH_20FPS) {
+		if (now - lasTime.current < REFRESH_RATE) {
 			return;
 		}
 		lasTime.current = now;
 
-		if (e.clientX > window.innerWidth / 2 || e.clientX < 70) {
+		if (e.clientX > MAX_WIDTH || e.clientX < MIN_WIDTH) {
 			return;
 		}
 
-		const sidebarWidth =
-			Math.max(e.clientX, 200) && Math.min(e.clientX, window.innerWidth / 2);
+		const sidebarWidth = Math.max(Math.min(e.clientX, MAX_WIDTH), MIN_WIDTH);
 		sidebarRef.current.style.width = `${sidebarWidth}px`;
-		// console.log(parseInt(sidebarRef.current.style.width) === width);
-		// if (!localStorage || parseInt(sidebarRef.current.style.width) === width) {
-		// 	console.log(sidebarWidth === width);
-		// 	return;
-		// }
-		// localStorage.setItem("sidebarWidth", String(sidebarWidth));
-	}, []);
+
+		if (localStorage) {
+			localStorage.setItem("sidebarWidth", String(sidebarWidth));
+		}
+	};
 
 	const handleMouseUp = () => {
 		if (!isResizing.current) {
@@ -59,17 +52,14 @@ function useResize() {
 		document.removeEventListener("mouseup", handleMouseUp);
 	};
 
-	const handleMousedown = useCallback(
-		(e: MouseEvent) => {
-			e.stopPropagation();
-			e.preventDefault();
+	const handleMousedown = (e: MouseEvent) => {
+		e.stopPropagation();
+		e.preventDefault();
 
-			document.addEventListener("mousemove", handleMouseMove);
-			document.addEventListener("mouseup", handleMouseUp);
-			isResizing.current = true;
-		},
-		[handleMouseMove, handleMouseUp],
-	);
+		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mouseup", handleMouseUp);
+		isResizing.current = true;
+	};
 
 	return { sidebarRef, handleMousedown };
 }
