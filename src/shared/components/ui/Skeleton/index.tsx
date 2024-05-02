87b757eb3ec5274bfd736d5e8a40a79427b5da51
Artur Wójtowicz <101038373+someUsernam@users.noreply.github.com>
2024-05-02 18:51:28 +0200

    REFACTOR: Refactor Skeleton component styles (#48)
    Co-authored-by: Artur Wojtowicz <arturwojtowik@gmail.com>

--- src/shared/components/ui/Skeleton/index.tsx
+++ src/shared/components/ui/Skeleton/index.tsx
@@ -1,16 +1,18 @@
 import { ComponentProps } from "react";
 
+const base = "animate-pulse ";
+
 const variants = {
 	"text-xs": "",
 	"text-sm": "",
-	text: "bg-neutral-700 w-[10ch] h-5 rounded-full animate-pulse",
-	"text-lg": "bg-neutral-700 w-[15ch] h-6 rounded-full animate-pulse",
-	"text-xl": "bg-neutral-600 w-[85%] h-6 rounded-full animate-pulse",
-	"square-sm": "w-12 h-12 rounded-sm dark:bg-neutral-700 aspect-square",
-	square: "bg-neutral-600 w-20 animate-pulse rounded-sm aspect-square",
-	"square-lg": "bg-neutral-700 w-full animate-pulse rounded-sm aspect-square",
-	"header-sm": "bg-neutral-700 w-1/5 h-5 rounded-full animate-pulse",
-	header: "w-2/5 rounded-full h-14 animate-pulse dark:bg-neutral-700",
+	text: `${base} bg-neutral-700 w-[10ch] h-5 rounded-full`,
+	"text-lg": `${base} bg-neutral-700 w-[15ch] h-6 rounded-full`,
+	"text-xl": `${base} bg-neutral-600 w-[85%] h-6 rounded-full`,
+	"square-sm": `${base} w-12 h-12 rounded-sm dark:bg-neutral-700 aspect-square`,
+	square: `${base} bg-neutral-600 w-20 rounded-sm aspect-square`,
+	"square-lg": `${base} bg-neutral-700 w-full rounded-sm aspect-square`,
+	"header-sm": `${base} bg-neutral-700 w-1/5 h-5 rounded-full `,
+	header: `${base} w-2/5 rounded-full h-14 dark:bg-neutral-700`,
 	badge: "",
 	"badge-lg": "",
 };
